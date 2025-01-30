fa906f5de1d8037c80a9f222963f185ad1ad0542
amrKhaled2024 <168763877+amrKhaled2024@users.noreply.github.com>
2025-01-30 21:36:36 +0200

    Virtual Keyboard.py

--- Virtual Keyboard.py
+++ Virtual Keyboard.py
@@ -6,7 +6,7 @@ import time
 
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
 
